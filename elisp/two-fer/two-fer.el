079df7d23c434f66566146b277aac52371cb5dde
serycjon <jonas.serych@gmail.com>
2018-08-27 22:07:28 +0200

    finish two-fer

--- elisp/two-fer/two-fer.el
+++ elisp/two-fer/two-fer.el
@@ -4,6 +4,11 @@
 
 ;;; Code:
 
+(defun two-fer (&optional name)
+  "Returns a two-fer with given `name' or 'you' instead"
+  (let ((name (or name "you")))
+    (format "One for %s, one for me." name)))
+
 
 (provide 'two-fer)
 ;;; two-fer.el ends here
