84b63d7b5eb1cb3e5d911aef16b9b400551fb2b0
serycjon <jonas.serych@gmail.com>
2018-09-01 14:14:59 +0200

    add collatz solution

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,9 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz x = collatz_aux x 0
+  where collatz_aux x n
+          | x == 1 = Just n
+          | x <= 0 = Nothing
+          | x `mod` 2 == 0 = collatz_aux (x `div` 2) (n + 1)
+          | otherwise = collatz_aux (3 * x + 1) (n + 1)
