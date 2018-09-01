87b1938b88dae293007cc2575f4b42ff13bab5fe
serycjon <jonas.serych@gmail.com>
2018-09-01 14:31:43 +0200

    add perfect numbers solution

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -2,5 +2,16 @@ module PerfectNumbers (classify, Classification(..)) where
 
 data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 
+factors :: Int -> [Int]
+factors number = [x | x <- [1 .. number], number `mod` x == 0]
+
+aliquotSum :: Int -> Int
+aliquotSum number = sum $ init $ factors number
+
 classify :: Int -> Maybe Classification
-classify = error "You need to implement this function."
+classify number
+  | number <= 0 = Nothing
+  | sum == number = Just Perfect
+  | sum > number = Just Abundant
+  | sum < number = Just Deficient
+  where sum = aliquotSum number
