36e70ae46d6e4fcec20dd141b8f2f4c454a71b7f
serycjon <jonas.serych@gmail.com>
2018-09-03 10:54:02 +0200

    collatz without helper function

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,9 +1,8 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz x = collatz_aux x 0
-  where collatz_aux x n
-          | x == 1 = Just n
-          | x <= 0 = Nothing
-          | x `mod` 2 == 0 = collatz_aux (x `div` 2) (n + 1)
-          | otherwise = collatz_aux (3 * x + 1) (n + 1)
+collatz x
+  | x == 1 = Just 0
+  | x <= 0 = Nothing
+  | x `mod` 2 == 0 = fmap (1+) (collatz (x `div` 2))
+  | otherwise = fmap (1+) (collatz (3 * x + 1))
