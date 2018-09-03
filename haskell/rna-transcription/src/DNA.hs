a22577bbb3baef1d0be2995b741ca7ec8622c905
serycjon <jonas.serych@gmail.com>
2018-09-03 22:44:33 +0200

    even more crazy version

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -2,7 +2,7 @@ module DNA (toRNA) where
 
 
 toRNA :: String -> Maybe String
-toRNA dna = sequence $ map nucleotideToRNA dna
+toRNA dna = mapM nucleotideToRNA dna
   where nucleotideToRNA 'C' = Just 'G'
         nucleotideToRNA 'G' = Just 'C'
         nucleotideToRNA 'T' = Just 'A'
