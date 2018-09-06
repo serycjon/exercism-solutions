3d253d841ce2449d5821689ce54589d2f2921e49
serycjon <jonas.serych@gmail.com>
2018-09-06 21:47:47 +0200

    add: sum-of-multiples solution

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,12 @@
 module SumOfMultiples (sumOfMultiples) where
 
+import qualified Data.Set as Set
+
+multiplesUpTo :: Integer -> Integer -> (Set.Set Integer)
+multiplesUpTo limit x = Set.fromList [x * n | n <- [1..(limit `div` x)], x * n < limit]
+
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = error "You need to implement this function."
+sumOfMultiples factors limit = sum allMultiples
+  where allMultipleSets = map (multiplesUpTo limit) factors
+        completeMultiplesSet = foldl Set.union Set.empty allMultipleSets
+        allMultiples = Set.toList completeMultiplesSet
